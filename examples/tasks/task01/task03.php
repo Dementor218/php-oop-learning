b278e79a1b9d482748962f5cc2501081cff8eb54
Евгений Куроедов <e.kuroedov@nastart-web.ru>
2026-07-14 15:58:14 +0300

    Tasks 03

---
+++ examples/tasks/task01/task03.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+include __DIR__ . "/../../../vendor/autoload.php";
+
+use App\Week01\Tasks\Task03\Config;
+
+$config1 = new Config();
+$config2 = new Config();
+$config3 = new Config();
+$config4 = new Config();
+$config5 = new Config();
+
+echo $config5->getCount();
